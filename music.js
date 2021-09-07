56f57241d3ce249dd9f3bf9253f6f6e336bf4c37
KamoliddinBoy <kmirzaeev@gmail.com>
2021-09-07 22:14:43 +0300

    It is my web music player with vanilla JavaScript

---
+++ music.js
@@ -0,0 +1,117 @@
+const container = document.getElementById('container')
+const audio = document.getElementById('audio')
+const title = document.getElementById('title')
+const cover = document.getElementById('cover')
+const start = document.getElementById('start')
+const and = document.getElementById('end')
+const progressCont = document.getElementById('progress-container')
+const progress = document.getElementById('progress')
+const playBtn = document.getElementById('play')
+const prevBtn = document.getElementById('prev')
+const nextBtn = document.getElementById('next')
+
+
+//all songs
+const songs = [
+    'Ending - Isak Danielson',
+    'Enrique - Ring My Bells',
+    'Hummali & Navai',
+    'Luna - Gafur'
+]
+
+let indexSong = 0
+
+loadSong(songs[indexSong])
+
+//show song info
+function loadSong(song) {
+    title.textContent = song
+    audio.src = `music/${song}.mp3`
+    cover.src = `albom/${song}.jpg`
+}
+
+
+//playSong
+function playSong() {
+    container.classList.add('play')
+    playBtn.innerHTML = `<i class="fas fa-pause"></i>`
+    audio.play()
+}
+
+function pauseSong() {
+    container.classList.remove('play')
+    playBtn.innerHTML = `<i class="fas fa-play"></i>`
+    audio.pause()
+}
+
+//nextSong
+function nextSong() {
+    indexSong++
+    if(indexSong > songs.length - 1) {
+        indexSong = 0
+    }
+    loadSong(songs[indexSong])
+    playSong()
+}
+
+//prevSong
+function prevSong() {
+    indexSong--
+    if(indexSong < 0) {
+        indexSong = songs.length - 1
+    }
+    loadSong(songs[indexSong])
+    playSong()
+}
+
+//setProgress
+function setProgress(e) {
+    // const curTime = e.srcElement.currentTime
+    // const duration = e.srcElement.duration
+
+
+
+    const { currentTime, duration} = e.srcElement
+    const currentTimeMusic = currentTime
+    const durationMusic = duration
+    const progressPresent = (currentTimeMusic / durationMusic) *100
+    progress.style.width = `${progressPresent}%`
+
+    let minutes = Math.floor(durationMusic / 60)
+    let seconds = Math.floor(durationMusic % 60)
+    end.textContent = `${minutes}:${(seconds = seconds < 10 ? '0' + seconds : seconds)}`
+
+    let currentMinutes = Math.floor(currentTimeMusic / 60)
+    let currentSecond = Math.floor(currentTimeMusic % 60)
+    start.textContent = `${currentMinutes}:${(currentSecond = currentSecond < 10 ? '0' + currentSecond : currentSecond)}`
+}
+
+function setProgressTime(e) {
+    const width = this.clientWidth
+    const clientX = e.offsetX
+    const duration = audio.duration
+    audio.currentTime = (clientX / width) * duration
+}
+
+
+
+
+
+
+//events
+playBtn.addEventListener('click', ()=> {
+    const isPlaying = container.classList.contains('play')
+
+    if(isPlaying) {
+        pauseSong()
+    } else {
+        playSong()
+    }
+})
+
+
+nextBtn.addEventListener('click', nextSong)
+prevBtn.addEventListener('click', prevSong)
+audio.addEventListener('timeupdate', setProgress)
+progressCont.addEventListener('click', setProgressTime)
+audio.addEventListener('end', nextSong)
